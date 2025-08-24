3b06b2945ce4070ffac4b2231563b829f18bc71c
Chakiel Zero YouTube <136051930+Chakielzero@users.noreply.github.com>
2025-08-24 10:26:32 -0400

    Update listas.js

--- android/assets/js/listas.js
+++ android/assets/js/listas.js
@@ -111,6 +111,14 @@ const juegos = [
 
     },       
     {
+        id: "Counter Strike 1.6",
+        imgSrc: "https://cdn.jsdelivr.net/gh/Chakielzero/imagenes@main/android/mmj.webp",
+        altText: "Counter Strike 1.6",
+        title: "Counter Strike 1.6 Port.apk",
+        downloadLink2: "https://terabox.com/s/1c_RfGD7OUMV65qmhxeqc5g" // Segundo enlace de descarga
+
+    },       
+    {
         id: "Cuphead Mobile",
         imgSrc: "https://cdn.jsdelivr.net/gh/Chakielzero/imagenes@main/android/cuphead.webp",
         altText: "Cuphead",
